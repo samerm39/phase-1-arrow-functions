f0e7859a1eafff237226a274824b743bce1d698a
Samer Mobarak <samermobarak888@gmail.com>
2023-08-09 20:20:19 -0400

    arrow_functions

---
+++ index.js
@@ -0,0 +1,10 @@
+//test 1 & 2
+function divide(number1, number2) {
+    return number1/number2;
+}
+
+//test 3 & 4
+const square = number => number * number;
+
+//test 5 & 6
+const add = (number1, number2) => number1 + number2;
